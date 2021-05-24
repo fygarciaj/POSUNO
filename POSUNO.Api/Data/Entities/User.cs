d7719bfab04dcef54001e55249bef4550505e564
Fabian Yesith Garcia Jurado <fygarciaj@users.noreply.github.com>
2021-05-23 23:23:38 -0500

    Create DB

---
+++ POSUNO.Api/Data/Entities/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POSUNO.Api.Data.Entities
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public String Email { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public String FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public String LastName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        [MinLength(6)]
+        public String Password { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+
+    }
+}
